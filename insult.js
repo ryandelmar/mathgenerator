dd43f59793cbc52a851e4cd357d56e41bb845b64
ryandelmar <30875224+ryandelmar@users.noreply.github.com>
2017-08-21 09:19:08 -1000

    Create insult.js

---
+++ insult.js
@@ -0,0 +1,5 @@
+function listInsults() {
+var listInsults = ["rotton avacado", "wanna hear a joke; you", "the solar eclipse is the only reason people stopped looking at you", "you look like you bathe behind the dumpster behind the abortion clinic "];
+var insultGenerator = (Math.floor(Math.random()*4));
+window.alert("" + listInsults[insultGenerator]);
+}
