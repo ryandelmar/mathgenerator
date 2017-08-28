294586c2d3d58f9400da4b31c1ae2bc4e6b76638
ryandelmar <30875224+ryandelmar@users.noreply.github.com>
2017-08-28 08:43:59 -1000

    Create changingthename.js

---
+++ changingthename.js
@@ -0,0 +1,10 @@
+var newName = prompt("What is his new first name?");
+
+var cole = {
+  firstname:"Cole",
+  lastname:"House",
+  age: 15,
+  grade: 10
+}
+
+window.alert(cole.firstname = newName);
